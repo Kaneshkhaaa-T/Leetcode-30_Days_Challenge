edb4c07c06704f2fcac7a100200e206e1381a2a8
Kaneshkhaaa-T <kaneshkhaoffc@gmail.com>
2025-11-11 21:12:45 +0530

    Time: 1 ms (49.17%), Space: 42.4 MB (7.95%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        
+        while(num/10!=0){
+            int sum = 0;
+            while(num!=0){
+                int mod = num%10;
+                sum = sum + mod;
+                num = num/10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
