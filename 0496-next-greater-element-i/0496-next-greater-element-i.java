c583cb9c2b9283d83a5c11cc7fd0bc16491563a3
Kaneshkhaaa-T <kaneshkhaoffc@gmail.com>
2026-01-07 22:21:31 +0530

    Time: 2 ms (98.57%), Space: 45.5 MB (22.69%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -17,15 +17,4 @@ class Solution {
 
         return nums1;
     }
-
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
-                writer.write("0");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }));
-    }
 }
