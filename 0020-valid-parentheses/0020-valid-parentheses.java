45636882feded6299348aab10d292f8323c3a63d
Kaneshkhaaa-T <kaneshkhaoffc@gmail.com>
2026-01-14 19:21:21 +0530

    Time: 5 ms (17.03%), Space: 43.8 MB (8.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -20,12 +20,4 @@ class Solution {
         if(!st.isEmpty()) return false;
         return true;
     }
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
 }
