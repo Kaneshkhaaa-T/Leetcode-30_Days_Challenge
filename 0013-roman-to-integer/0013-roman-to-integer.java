1ae5a90b6a66bb0eb0092346338988dfffa88c3b
Kaneshkhaaa-T <kaneshkhaoffc@gmail.com>
2026-01-20 18:26:25 +0530

    Time: 3 ms (74.3%), Space: 47.1 MB (12.79%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,12 +1,5 @@
 class Solution {
-     static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-     }
+    
     public int romanToInt(String s) {
         int n = s.length();
         int num = 0;
